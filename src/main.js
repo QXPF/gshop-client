34faedffc02e252ca1137fa8296169a624d3d913
QXPF <123687886@qq.com>
2019-07-31 23:39:29 +0800

    外卖静态页面提交

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+/*
+入口JS
+*/
+
 import Vue from 'vue'
 import App from './App'
 import router from './router'
@@ -9,7 +13,11 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  render: h => h(App),
   router,
-  components: { App },
-  template: '<App/>'
+
+  // 声明模版
+  // template: '<App/>',
+  // 注册成组件
+  // components: { App }
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,36 @@
+/*路由模块对象 */
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import MSite from '../pages/MSite/MSite'
+import Search from '../pages/Search/Search'
+import Order from '../pages/Order/Order'
+import Profile from '../pages/Profile/Profile'
+//声明使用插件
 Vue.use(Router)
 
 export default new Router({
+  //所有路由
   routes: [
+    // 重定向：
+    // / 是默认的路径，页面第一打开的时候，就会访问这个路径
+    // 当这个路径匹配成功后，通过指定的 redirect 就可以重定向到其他路由了
+    { path: '/', redirect: '/msite' },
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      path: "/msite",
+      component: MSite,
+    },
+    {
+      path: "/search",
+      component: Search,
+    },
+    {
+      path: "/profile",
+      component: Profile,
+    },
+    {
+      path: "/order",
+      component: Order,
+    },
+
   ]
 })
